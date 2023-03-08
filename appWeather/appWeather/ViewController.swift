f59def374b55ce818491336b1976afb04987d2a1
Vadim Baburin <>
2023-03-08 23:08:59 +0300

    new settings for temperature and location

--- appWeather/appWeather/ViewController.swift
+++ appWeather/appWeather/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchBar.delegate = self
     }
-
 
 }
 
+extension ViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let urlString =  "http://api.weatherstack.com/current?access_key=2b5a6e49306834779ea76ba4c518ee14&query=\(searchBar.text!)"
+        let url = URL(string: urlString)
+        
+        var locationName: String?
+        var temperature: Double?
+        
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers)
+                    as! [String : AnyObject]
+                
+                if let location = json["location"] {
+                    locationName = location["name"] as? String
+                }
+                
+                if let current = json["current"] {
+                    temperature = current["temperature"] as? Double
+                }
+                
+            }
+            catch let jsonError {
+                print(jsonError)
+            }
+            
+        }
+        task.resume()
+    }
+}
