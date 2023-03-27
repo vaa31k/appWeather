68cbf234bc31aee7b78f0749147141c15015e710
Vadim Baburin <>
2023-03-27 16:39:19 +0300

    new image and settings

--- appWeather/appWeather/ViewController.swift
+++ appWeather/appWeather/ViewController.swift
@@ -13,9 +13,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var searchBar: UISearchBar!
     
+    //var locationName: String?
+    //var temperature: Double?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        searchBar.delegate = self
+        //searchBar.delegate = self
+        
     }
 
 }
@@ -25,7 +29,12 @@ extension ViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchBar.resignFirstResponder()
         
-        let urlString =  "http://api.weatherstack.com/current?access_key=2b5a6e49306834779ea76ba4c518ee14&query=\(searchBar.text!.replacingOccurrences(of: " ", with: "%20"))".encodeUrl
+        guard let searchBarUnwrappedText = searchBar.text else {
+            print("text = nil")
+            return
+        }
+        
+        let urlString =  "http://api.weatherstack.com/current?access_key=2b5a6e49306834779ea76ba4c518ee14&query=\(searchBarUnwrappedText.replacingOccurrences(of: " ", with: "%20"))".encodeUrl
         let url = URL(string: urlString)
         
         guard let unwrappedUrl = url else {
@@ -33,41 +42,30 @@ extension ViewController: UISearchBarDelegate {
             return
         }
         
-        var locationName: String?
-        var temperature: Double?
-        var errorHasOccured: Bool = false
-    
+        
+      //  var errorHasOccured: Bool = false
+        
         let task = URLSession.shared.dataTask(with: unwrappedUrl) { [weak self] (data, response, error) in
             do {
-                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers)
+                guard let unwrappedData = data else {
+                    print("data = nil")
+                    return
+                }
+                
+                let json = try JSONSerialization.jsonObject(with: unwrappedData, options: .mutableContainers)
                 as! [String : AnyObject]
                 
-                if let _ = json["error"] {
-                    errorHasOccured = true
+                if let error = json["error"] {
+                    self?.fillLabelError(errorInfo: error["info"] as? String)
+                   // errorHasOccured = true
                 }
                 
-                if let location = json["location"] {
-                    locationName = location["name"] as? String
-                }
-                
-                if let current = json["current"] {
-                    temperature = current["temperature"] as? Double
-                }
-                
-                DispatchQueue.main.async {
-                    if errorHasOccured {
-                        self?.cityLabel.text = "Error has occured"
-                        self?.temperatureLabel.isHidden = true
-                        
-                    } else {
-                        
-                        self?.cityLabel.text = locationName
-                        self?.temperatureLabel.text = "\(temperature!)"
-                        
-                        self?.temperatureLabel.isHidden = false
-                        
-                    }
-
+                if let location = json["location"],
+                   let current = json["current"] {
+                    self?.fillLabels(locationName: location["name"] as? String,
+                                     temperature: current["temperature"] as? Double
+                    )
+                    
                 }
                 
             }
@@ -76,5 +74,31 @@ extension ViewController: UISearchBarDelegate {
             }
         }
         task.resume()
+        
+    }
+    
+    func fillLabelError(errorInfo: String?) {
+
+        DispatchQueue.main.async {
+        
+            self.cityLabel.text = errorInfo
+            self.temperatureLabel.isHidden = true
+            
+        }
+    }
+    
+    func fillLabels(locationName: String?, temperature: Double?) {
+
+        DispatchQueue.main.async {
+            self.cityLabel.text = locationName
+            
+            guard let unwrappedTemperature = temperature else {
+                print("temperature = nil")
+                return
+            }
+            
+            self.temperatureLabel.text = "\(unwrappedTemperature)"
+            self.temperatureLabel.isHidden = false
+        }
     }
 }
